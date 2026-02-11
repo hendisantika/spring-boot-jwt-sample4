6307b76bcd10ed08e2bdb9d9b59bc23484bf1f8d
hendisantika <hendisantika@yahoo.co.id>
2026-02-12 06:49:23 +0700

    feat: Add AuthenticationResponse Payload🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/jwtsample4/payload/response/AuthenticationResponse.java
@@ -0,0 +1,37 @@
+package id.my.hendisantika.jwtsample4.payload.response;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-jwt-sample4
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 12/02/26
+ * Time: 06.48
+ * To change this template use File | Settings | File Templates.
+ */
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class AuthenticationResponse {
+    private Long id;
+    private String email;
+    private List<String> roles;
+
+    @JsonProperty("access_token")
+    private String accessToken;
+    @JsonProperty("refresh_token")
+    private String refreshToken;
+    @JsonProperty("token_type")
+    private String tokenType;
+}
