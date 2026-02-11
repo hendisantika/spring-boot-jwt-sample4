28430fe45ee55174069e0d48a9a90c006b0e3207
hendisantika <hendisantika@yahoo.co.id>
2026-02-12 06:39:42 +0700

    feat: Add User Entity🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/jwtsample4/entity/User.java
@@ -0,0 +1,82 @@
+package id.my.hendisantika.jwtsample4.entity;
+
+import id.my.hendisantika.jwtsample4.enums.Role;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-jwt-sample4
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 12/02/26
+ * Time: 06.37
+ * To change this template use File | Settings | File Templates.
+ */
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity
+@Table(name = "_user")
+public class User implements UserDetails { // make our app User a spring security User
+    /*
+        we have two options : implements the UserDetails interface or create a user class that extends User spring class which also
+        implements UserDetails
+     */
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String firstname;
+    private String lastname;
+    private String email;
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    // we should return a list of roles
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return role.getAuthorities();
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
