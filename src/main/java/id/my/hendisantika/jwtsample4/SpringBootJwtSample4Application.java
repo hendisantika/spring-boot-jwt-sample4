6c9b48ce60143d61477feab5c2423b91a165babd
hendisantika <hendisantika@yahoo.co.id>
2026-02-12 06:33:48 +0700

    feat: Init Project🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/jwtsample4/SpringBootJwtSample4Application.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.jwtsample4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJwtSample4Application {
+
+    static void main(String[] args) {
+        SpringApplication.run(SpringBootJwtSample4Application.class, args);
+    }
+
+}
---
+++ src/test/java/id/my/hendisantika/jwtsample4/SpringBootJwtSample4ApplicationTests.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.jwtsample4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJwtSample4ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
