8d2da6307a734dd7bab94d4ab4ff272416387923
hendisantika <hendisantika@yahoo.co.id>
2026-02-12 06:56:57 +0700

    feat: Add createRefreshToken in RefreshTokenService🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/jwtsample4/service/RefreshTokenService.java
+++ src/main/java/id/my/hendisantika/jwtsample4/service/RefreshTokenService.java
@@ -1,11 +1,17 @@
 package id.my.hendisantika.jwtsample4.service;
 
+import id.my.hendisantika.jwtsample4.entity.RefreshToken;
+import id.my.hendisantika.jwtsample4.entity.User;
 import id.my.hendisantika.jwtsample4.repository.RefreshTokenRepository;
 import id.my.hendisantika.jwtsample4.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.util.Base64;
+import java.util.UUID;
 
 /**
  * Created by IntelliJ IDEA.
@@ -30,4 +36,15 @@ public class RefreshTokenService {
     private long refreshExpiration;
     @Value("${application.security.jwt.refresh-token.cookie-name}")
     private String refreshTokenName;
+
+    public RefreshToken createRefreshToken(Long userId) {
+        User user = userRepository.findById(userId).orElseThrow(() -> new RuntimeException("User not found"));
+        RefreshToken refreshToken = RefreshToken.builder()
+                .revoked(false)
+                .user(user)
+                .token(Base64.getEncoder().encodeToString(UUID.randomUUID().toString().getBytes()))
+                .expiryDate(Instant.now().plusMillis(refreshExpiration))
+                .build();
+        return refreshTokenRepository.save(refreshToken);
+    }
 }
