adb411dd35aa732e2a998c9346b1b8b001bcf225
hendisantika <hendisantika@yahoo.co.id>
2026-02-12 06:52:07 +0700

    feat: Add JwtService🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/jwtsample4/service/JwtService.java
@@ -0,0 +1,29 @@
+package id.my.hendisantika.jwtsample4.service;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-jwt-sample4
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 12/02/26
+ * Time: 06.51
+ * To change this template use File | Settings | File Templates.
+ */
+@Service
+public class JwtService {
+
+    @Value("${application.security.jwt.secret-key}")
+    private String secretKey;
+
+    @Value("${application.security.jwt.expiration}")
+    private long jwtExpiration;
+    @Value("${application.security.jwt.refresh-token.expiration}")
+    private long refreshExpiration;
+    @Value("${application.security.jwt.cookie-name}")
+    private String jwtCookieName;
+}
