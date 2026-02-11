139dbd875df8931ec4ddc79beacca76be15e7900
hendisantika <hendisantika@yahoo.co.id>
2026-02-12 06:51:27 +0700

    feat: Add authenticate in AuthenticationService🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/jwtsample4/service/AuthenticationService.java
+++ src/main/java/id/my/hendisantika/jwtsample4/service/AuthenticationService.java
@@ -2,11 +2,13 @@ package id.my.hendisantika.jwtsample4.service;
 
 import id.my.hendisantika.jwtsample4.entity.User;
 import id.my.hendisantika.jwtsample4.enums.TokenType;
+import id.my.hendisantika.jwtsample4.payload.request.AuthenticationRequest;
 import id.my.hendisantika.jwtsample4.payload.request.RegisterRequest;
 import id.my.hendisantika.jwtsample4.payload.response.AuthenticationResponse;
 import id.my.hendisantika.jwtsample4.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -56,6 +58,27 @@ public class AuthenticationService {
                 .id(user.getId())
                 .refreshToken(refreshToken.getToken())
                 .roles(roles)
+                .tokenType(TokenType.BEARER.name())
+                .build();
+    }
+
+    public AuthenticationResponse authenticate(AuthenticationRequest request) {
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(request.getEmail(), request.getPassword()));
+
+        var user = userRepository.findByEmail(request.getEmail()).orElseThrow(() -> new IllegalArgumentException("Invalid email or password."));
+        var roles = user.getRole().getAuthorities()
+                .stream()
+                .map(SimpleGrantedAuthority::getAuthority)
+                .toList();
+        var jwt = jwtService.generateToken(user);
+        var refreshToken = refreshTokenService.createRefreshToken(user.getId());
+        return AuthenticationResponse.builder()
+                .accessToken(jwt)
+                .roles(roles)
+                .email(user.getEmail())
+                .id(user.getId())
+                .refreshToken(refreshToken.getToken())
                 .tokenType(TokenType.BEARER.name())
                 .build();
     }
