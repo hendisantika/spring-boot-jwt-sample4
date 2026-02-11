8e7b53db7da11a93e9a2072e9683f5a8df04b34f
hendisantika <hendisantika@yahoo.co.id>
2026-02-12 06:48:42 +0700

    feat: Add RegisterRequest Payload🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/jwtsample4/payload/request/RegisterRequest.java
@@ -0,0 +1,41 @@
+package id.my.hendisantika.jwtsample4.payload.request;
+
+import id.my.hendisantika.jwtsample4.enums.Role;
+import id.my.hendisantika.jwtsample4.validation.StrongPassword;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-jwt-sample4
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 12/02/26
+ * Time: 06.48
+ * To change this template use File | Settings | File Templates.
+ */
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class RegisterRequest {
+    @NotBlank(message = "firstname is required")
+    private String firstname;
+    @NotBlank(message = "lastname is required")
+    private String lastname;
+    @NotBlank(message = "email is required")
+    @Email(message = "email format is not valid")
+    private String email;
+    @NotBlank(message = "password is required")
+    @StrongPassword
+    private String password;
+    @NotNull
+    private Role role;
+}
