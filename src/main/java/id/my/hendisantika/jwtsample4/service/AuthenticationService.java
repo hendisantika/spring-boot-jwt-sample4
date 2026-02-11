5615cc980843533599c088ff4f73ea75a5d407f9
hendisantika <hendisantika@yahoo.co.id>
2026-02-12 06:51:09 +0700

    feat: Add register in AuthenticationService🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/jwtsample4/service/AuthenticationService.java
+++ src/main/java/id/my/hendisantika/jwtsample4/service/AuthenticationService.java
@@ -1,8 +1,13 @@
 package id.my.hendisantika.jwtsample4.service;
 
+import id.my.hendisantika.jwtsample4.entity.User;
+import id.my.hendisantika.jwtsample4.enums.TokenType;
+import id.my.hendisantika.jwtsample4.payload.request.RegisterRequest;
+import id.my.hendisantika.jwtsample4.payload.response.AuthenticationResponse;
 import id.my.hendisantika.jwtsample4.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -27,4 +32,31 @@ public class AuthenticationService {
     private final UserRepository userRepository;
     private final AuthenticationManager authenticationManager;
     private final RefreshTokenService refreshTokenService;
+
+    public AuthenticationResponse register(RegisterRequest request) {
+        var user = User.builder()
+                .firstname(request.getFirstname())
+                .lastname(request.getLastname())
+                .email(request.getEmail())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .role(request.getRole())
+                .build();
+        user = userRepository.save(user);
+        var jwt = jwtService.generateToken(user);
+        var refreshToken = refreshTokenService.createRefreshToken(user.getId());
+
+        var roles = user.getRole().getAuthorities()
+                .stream()
+                .map(SimpleGrantedAuthority::getAuthority)
+                .toList();
+
+        return AuthenticationResponse.builder()
+                .accessToken(jwt)
+                .email(user.getEmail())
+                .id(user.getId())
+                .refreshToken(refreshToken.getToken())
+                .roles(roles)
+                .tokenType(TokenType.BEARER.name())
+                .build();
+    }
 }
