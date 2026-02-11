b51063ef8291319b92d6cb1782708851887a4201
hendisantika <hendisantika@yahoo.co.id>
2026-02-12 06:40:35 +0700

    feat: Add TokenType Enum🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/jwtsample4/enums/TokenType.java
@@ -0,0 +1,16 @@
+package id.my.hendisantika.jwtsample4.enums;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-jwt-sample4
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 12/02/26
+ * Time: 06.40
+ * To change this template use File | Settings | File Templates.
+ */
+public enum TokenType {
+    BEARER
+}
