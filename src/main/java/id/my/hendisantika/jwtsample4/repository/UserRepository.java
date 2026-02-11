b19661d93ce89d46a914cc9d667f1d18ef1060d1
hendisantika <hendisantika@yahoo.co.id>
2026-02-12 06:44:01 +0700

    feat: Add UserRepository🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/jwtsample4/repository/UserRepository.java
@@ -0,0 +1,21 @@
+package id.my.hendisantika.jwtsample4.repository;
+
+import id.my.hendisantika.jwtsample4.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-jwt-sample4
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 12/02/26
+ * Time: 06.43
+ * To change this template use File | Settings | File Templates.
+ */
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+}
