0dc94076638c465963f8c9620af1d73bd8ace087
hendisantika <hendisantika@yahoo.co.id>
2026-02-12 06:46:39 +0700

    feat: Add AuthenticationService🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/jwtsample4/service/AuthenticationService.java
@@ -0,0 +1,30 @@
+package id.my.hendisantika.jwtsample4.service;
+
+import id.my.hendisantika.jwtsample4.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-jwt-sample4
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 12/02/26
+ * Time: 06.46
+ * To change this template use File | Settings | File Templates.
+ */
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class AuthenticationService {
+    private final PasswordEncoder passwordEncoder;
+    private final JwtService jwtService;
+    private final UserRepository userRepository;
+    private final AuthenticationManager authenticationManager;
+    private final RefreshTokenService refreshTokenService;
+}
