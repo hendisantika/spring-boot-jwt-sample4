359c729c10ffded46b0085049627d5e810c915a9
hendisantika <hendisantika@yahoo.co.id>
2026-02-12 07:00:35 +0700

    feat: Add CustomAccessDeniedHandler🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/jwtsample4/config/CustomAccessDeniedHandler.java
@@ -0,0 +1,52 @@
+package id.my.hendisantika.jwtsample4.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import id.my.hendisantika.jwtsample4.handler.ErrorResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.time.Instant;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-jwt-sample4
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 12/02/26
+ * Time: 06.59
+ * To change this template use File | Settings | File Templates.
+ */
+@Component
+@Slf4j
+public class CustomAccessDeniedHandler implements AccessDeniedHandler {
+    @Override
+    public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException {
+        log.error("Access denied error: {}", accessDeniedException.getMessage());
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+
+        ErrorResponse body = ErrorResponse.builder()
+                .status(HttpServletResponse.SC_FORBIDDEN)
+                .error("Forbidden")
+                .timestamp(Instant.now())
+                .message(accessDeniedException.getMessage())
+                .path(request.getServletPath())
+                .build();
+        final ObjectMapper mapper = new ObjectMapper();
+        // register the JavaTimeModule, which enables Jackson to support Java 8 and higher date and time types
+        mapper.registerModule(new JavaTimeModule());
+        // ask Jackson to serialize dates as strings in the ISO 8601 format
+        mapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
+        mapper.writeValue(response.getOutputStream(), body);
+    }
+}
