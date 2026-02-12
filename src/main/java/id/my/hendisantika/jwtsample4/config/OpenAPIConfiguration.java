99366c95f4e23db1b281887c93a607024334193d
hendisantika <hendisantika@yahoo.co.id>
2026-02-12 07:04:54 +0700

    feat: Add OpenAPIConfiguration🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/jwtsample4/config/OpenAPIConfiguration.java
@@ -0,0 +1,71 @@
+package id.my.hendisantika.jwtsample4.config;
+
+import id.my.hendisantika.jwtsample4.handler.ErrorResponse;
+import io.swagger.v3.core.converter.AnnotatedType;
+import io.swagger.v3.core.converter.ModelConverters;
+import io.swagger.v3.core.converter.ResolvedSchema;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.servers.Server;
+import org.springdoc.core.customizers.OpenApiCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-jwt-sample4
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 12/02/26
+ * Time: 07.03
+ * To change this template use File | Settings | File Templates.
+ */
+@Configuration
+@OpenAPIDefinition(
+        info = @Info(
+                contact = @Contact(
+                        name = "Hendi Santika",
+                        email = "hendisantika@yahoo.co.id"
+                ),
+                title = "Spring Security 7+ APP",
+                description = "Spring Boot 4+ Spring Security 7+ example",
+                version = "0.0.1"
+        ),
+        servers = {
+                @Server(
+                        description = "Development",
+                        url = "http://localhost:8080"
+
+                )
+        },
+        security = {
+                @SecurityRequirement(
+                        name = "bearerAuth"
+                )
+        }
+)
+@SecurityScheme(
+        name = "bearerAuth",
+        description = "JWT auth description",
+        scheme = "bearer",
+        type = SecuritySchemeType.HTTP,
+        bearerFormat = "JWT",
+        in = SecuritySchemeIn.HEADER
+)
+
+public class OpenAPIConfiguration {
+    @Bean
+    public OpenApiCustomizer schemaCustomizer() {
+        ResolvedSchema resolvedSchema = ModelConverters.getInstance()
+                .resolveAsResolvedSchema(new AnnotatedType(ErrorResponse.class));
+        return openApi -> openApi
+                .schema(resolvedSchema.schema.getName(), resolvedSchema.schema);
+    }
+}
